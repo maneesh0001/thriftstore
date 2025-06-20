e38ed70f254a8286c29be916b3f237565a29ed38
maneesh0001 <ajcen0006@gmail.com>
2025-06-20 22:22:28 +0545

    feat: add failure classes for error handling in the application

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+ 
+abstract class Failure extends Equatable {
+  final String message;
+ 
+  const Failure({required this.message});
+ 
+  @override
+  List<Object?> get props => [message];
+}
+ 
+ 
+class LocalDatabaseFailure extends Failure {
+  const LocalDatabaseFailure({required super.message});
+}
+class ApiFailure extends Failure {
+  const ApiFailure({required super.message});
+}
