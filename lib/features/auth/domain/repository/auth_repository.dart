3331fc6671da71eda3024a6726de412ae54c5427
maneesh0001 <ajcen0006@gmail.com>
2025-08-03 19:42:29 +0545

    add auth repo

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,9 +1,26 @@
-import 'package:dartz/dartz.dart';
-import 'package:thrift_store/core/error/failure.dart' show Failure;
-import 'package:thrift_store/features/auth/domain/entity/user_entity.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:thrift_store/core/error/failure.dart' show Failure;
+// import 'package:thrift_store/features/auth/domain/entity/user_entity.dart';
 
  
+// abstract interface class IAuthRepository {
+//   Future<Either<Failure, void>> createAccount(UserEntity user);
+//   Future<Either<Failure, String>> loginToAccount(String email, String password);
+// }
+
+
+import 'dart:io';
+import 'package:dartz/dartz.dart';
+import 'package:thrift_store/core/error/failure.dart';
+import 'package:thrift_store/features/auth/domain/entity/auth_entity.dart';
+import 'package:thrift_store/features/auth/domain/entity/login_response_entity.dart';
+import 'package:thrift_store/features/auth/domain/entity/user_entity.dart';
+
+
 abstract interface class IAuthRepository {
   Future<Either<Failure, void>> createAccount(UserEntity user);
-  Future<Either<Failure, String>> loginToAccount(String email, String password);
+Future<Either<Failure, LoginResponse>> loginToAccount(String email, String password);
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+    Future<Either<Failure, String>> uploadProfilePicture(File file);
+    Future<Either<Failure, void>> updateUser(String name, String email,int age, String phone);
 }
